b14278541d374ecb88da3e73576685896c7680d4
Ajay Kumar Singh <ajaysonusingh@hotmail.com>
2017-07-22 18:02:13 +0530

    Adding search

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,7 +8,13 @@ import { EmployeePage } from '../employee/employee'
   templateUrl: 'home.html'
 })
 export class HomePage {
-  absentemployees:AbsentEmployee[]=[
+  absentemployees:AbsentEmployee[];
+  constructor(public navCtrl: NavController) {
+    this.initializeEmployees();
+  }
+
+  initializeEmployees(){
+    this.absentemployees=[
     {icon:"../../assets/icon/cheetah.jpg", name:"Ajay Kumar Singh", personalcode:"123", startdate:"22-July-2017"},
     {icon:"../../assets/icon/cheetah.jpg", name:"Juned Jahangirdar", personalcode:"456", startdate:"22-July-2017"},
     {icon:"../../assets/icon/cheetah.jpg", name:"Mayank Shekhar", personalcode:"678", startdate:"22-July-2017"},
@@ -18,12 +24,25 @@ export class HomePage {
     {icon:"../../assets/icon/cheetah.jpg", name:"Sai Kumar Chilamkoti", personalcode:"1617", startdate:"22-July-2017"},
     {icon:"../../assets/icon/cheetah.jpg", name:"Jacob Kochekkan", personalcode:"1819", startdate:"22-July-2017"}
   ];
-  constructor(public navCtrl: NavController) {
-    
   }
   openEmployee(item: AbsentEmployee) {
     this.navCtrl.push(EmployeePage, {
       item: item
     });
+  }
+
+  getEmployees(ev) {
+    // Reset items back to all of the items
+    this.initializeEmployees();
+
+    // set val to the value of the ev target
+    var val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.absentemployees = this.absentemployees.filter((item) => {
+        return (item.name.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
   }
 }
